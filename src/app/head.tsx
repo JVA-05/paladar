cbc5086cf2a48fe913a9f26668b385d3635b3ea6
JVA-05 <jorgevidaurreta791@gmail.com>
2025-07-19 14:58:11 -0400

    Add head.tsx with Google site-verification

---
+++ src/app/head.tsx
@@ -0,0 +1,12 @@
+// app/head.tsx
+export default function Head() {
+    return (
+      <>
+        <meta
+          name="google-site-verification"
+          content="adKAiLXS7UlsBBX0QLL5q6OYKu8Rrm2KrVuM5Jcetz0"
+        />
+      </>
+    );
+  }
+  
