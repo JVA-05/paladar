e9bb6ca704ca73568427e8859826c0ddcb9a4f95
JVA-05 <jorgevidaurreta791@gmail.com>
2025-07-28 13:58:30 -0400

    fuera cookie

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import "@/app/globals.css";                   // tus estilos globales
 import "leaflet/dist/leaflet.css";            // estilos de Leaflet
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { AuthProvider } from "@/context/AuthContext";
 import Navbar from "@/app/components/Navbar/Navbar";
 import Footer from "@/app/components/Footer/Footer";
 
@@ -19,17 +18,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="es" className={`${inter.className} h-full bg-amber-50`}>
       <body className="flex min-h-screen flex-col bg-amber-50">
-        <AuthProvider>
           {/* Header fijo de 4rem (h-16) */}
           <header className="fixed inset-x-0 top-0 h-16 bg-white shadow z-50">
             <Navbar />
           </header>
-
-          {/* Empuja todo el contenido 4rem hacia abajo */}
           <main className="flex-1 pt-16">{children}</main>
-
           <Footer />
-        </AuthProvider>
       </body>
     </html>
   );
